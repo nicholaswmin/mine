0f674537160284281f8612659ccef73da9e4ddcc
Nicholas Kyriakides <nik.kyriakides@gmail.com>
2024-07-19 18:30:34 +0300

    Create programs.sh

---
+++ programs.sh
@@ -0,0 +1,33 @@
+# apps/programs
+
+
+# Personal 
+
+# all the browsers
+brew install --cask google-chrome
+brew install --cask google-chrome-canary
+brew install --cask firefox
+
+# slack
+brew install --cask slack
+
+# whatsapp
+brew install --cask whatsapp
+
+# Work
+
+# zed code editor
+brew install --cask zed
+
+# Throttle your internet/localhost 
+npm install -g sitespeedio/throttle
+
+# github CLI
+brew install gh
+
+# Heroku CLI
+brew tap heroku/brew && brew install heroku
+
+
+
+
