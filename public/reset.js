9d2a66abe165b06bf10594f55ceed771b1d0712f
Nicholas Kyriakides <nik.kyriakides@gmail.com>
2024-12-16 18:53:18 +0200

    Update reset.js

--- public/reset.js
+++ public/reset.js
@@ -3,5 +3,5 @@ $$('.button').map(el => el.on('click', console.log))
 $('body').on('mouseenter', console.log) */
 
 Node.prototype.on = Node.prototype.addEventListener
-window.$ = document.querySelectorAll.bind(document)
+window.$ = document.querySelector.bind(document)
 window.$$ = (...a) => Array.from(document.querySelectorAll(...a))
